e83fe0c6244bd2374314181117656dd5619c7178
uverrr <yuki.giga.baseball1@gmail.com>
2018-06-19 00:20:57 +0900

    編集画面の作成

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import play.mvc.*;
-import views.html.index;
+import views.html.*;
 
 public class HomeController extends Controller {
 
     public Result index() {
         return Results.ok(index.render("Your new application is ready."));
+    }
+
+    public Result edit() {
+        return Results.ok(edit.render());
     }
 }
