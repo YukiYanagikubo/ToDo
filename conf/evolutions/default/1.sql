80bee4cd9a337c5b28a6c7768070421de510f85f
uverrr <yuki.giga.baseball1@gmail.com>
2018-06-21 02:00:06 +0900

    Todoリスト追加機能の作成

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,19 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table todo (
+  id                            bigint auto_increment not null,
+  text                          varchar(255),
+  deadline                      timestamp,
+  done                          boolean default false not null,
+  postdate                      timestamp not null,
+  constraint pk_todo primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists todo;
+
